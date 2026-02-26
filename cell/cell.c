baa8f7e981ddfdcedd41f83dc345d1e6d3b6399b
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-02-26 18:07:14 +0400

    fix conflict rebase after edit property set_min_near_cell()

--- cell/cell.c
+++ cell/cell.c
@@ -10,46 +10,51 @@ struct cell {
 // при неудаче возвращаем ошибку(иначе 0), везде даже в инициализации
 //
 // Создаём ячейку
-cell init_cell(void) {
-    /* TODO */
+cell_t init_cell(void) {
+    cell_t cell = calloc(sizeof(*cell), 1);
+    cell->m_mins_near = 0;
+    cell->m_is_min = false;
+    cell->m_is_flag = false;
+    return cell;
 }
 
 // Ставим флаг
 void up_flag_cell(cell_t cell) {
-    /* TODO */
+    (*cell).m_is_flag = true;
 }
 
 // Убираем флаг
 void down_flag_cell(cell_t cell) {
-    /* TODO */
+
+    (*cell).m_is_flag = true;
 }
 
 // Возвращаем есть ли флаг
 bool is_flag_cell(cell_t cell) {
-    /* TODO */
+    return (*cell).m_is_flag;
 }
 
 // Устанавливаем мину
 void up_min_cell(cell_t cell) {
-    /* TODO */
+    (*cell).m_is_min = true;
 }
 
 // Возвращаем есть ли на данной ячейке бомба
 bool is_min_cell(cell_t cell) {
-    /* TODO */
+    return (*cell).m_is_min;
 }
 
 // Записываем сколько мин вокруг
 void set_min_near_cell(cell_t cell, int count_min) {
-    /* TODO */
+    (*cell).m_mins_near = count_min;
 }
 
 // Получаем сколько мин вокруг
 int get_min_near_cell(cell_t cell) {
-    /* TODO */
+    return (*cell).m_mins_near;
 }
 
 // Освобождаем ресурсы
 void destroy_cell(cell_t cell) {
-    /* TODO */
+    free(cell);
 }
